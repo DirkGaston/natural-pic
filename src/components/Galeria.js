af1f5e55ca179232771079b132847e258fa7aeb3
DirkGaston <dirkgaston87@gmail.com>
2022-09-27 16:07:23 -0300

    feat: axios data fetch and gallery render

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "../assets/css/galeria.css";
 import Heart from "./Heart";
 
-export default function Home() {
+export default function Galeria() {
+  const [fotos, setFotos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("fotos.json")
+      .then((res) => {
+        console.log(res.data.photos);
+        setFotos(res.data.photos);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="galeria grid-columns-5 p-3">
-
+      {fotos.map((foto, i) => (
+        <div
+          className="foto"
+          style={{ backgroundImage: `url(${foto.src.medium})` }}
+        >
+          <Heart />
+          <p className="fotoDescripcion"> {foto.alt} </p>
+        </div>
+      ))}
     </div>
   );
 }
